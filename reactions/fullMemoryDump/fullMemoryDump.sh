c59a9c0d6c38921ed7833ae4cae85a5660484875
Andrew Longhorn <Andrew_Longhorn@McAfee.com>
2021-04-28 13:06:07 +1000

    add getFile.sh

--- reactions/fullMemoryDump/fullMemoryDump.sh
+++ reactions/fullMemoryDump/fullMemoryDump.sh
@@ -6,16 +6,16 @@
 # recommend you set up an FTP user with:
 #    read-only access to a directory of tools that can be downloaded (ie. /tools)
 #    write-only access (blind dropbox/upload) to a directory for evidence collection (ie. /evidence)
-ftpSite = '192.168.0.100'
-ftpUser = 'user'
-ftpPass = 'pass'
-evidenceDir = '/evidence'
+ftpSite='192.168.0.100'
+ftpUser='user'
+ftpPass='pass'
+evidenceDir='/evidence'
 
-date = `date -u "+%Y%m%dT%H%M%S000Z"`
+date=`date -u "+%Y%m%dT%H%M%S000Z"`
 # make sure no spaces or special characters in the tmpdir path
-tempDir = '/tmp'
-hostname = `hostname`
-tempFile = "${hostname}_${date}_MemoryDump"
+tempDir='/tmp'
+hostname=`hostname`
+tempFile="${hostname}_${date}_MemoryDump"
 
 dd if=/dev/mem of="${tempDir}/${tempFile}"
 ftp "${tempDir}/${tempFile}" "${ftpuser}:${ftppass}@${ftpSite}${evidenceDir}"
---
+++ reactions/getFile/getFile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# reaction parameters
+# set this up as a string input with the name "filename" - it's meant to be the full path to the file we want to get
+sourceFile="{{filename}}"
+destFileName=`echo "${sourceFile}" | sed "s/:/./g" | sed "s/\//_/g"`
+echo $destFileName > /test.out
+
+# Change these to suit your environment
+# recommend you set up an FTP user with:
+#    write-only access (blind dropbox/upload) to a directory for evidence collection (ie. /evidence)
+ftpSite='192.168.0.100'
+ftpUser='user'
+ftpPass='pass'
+evidenceDir='evidence'
+
+#
+# get unique timestamp and hostname for file output tagging
+#
+date=`date -u "+%Y%m%dT%H%M%S000Z"`
+hostname=`hostname`
+destFile="${date}_${hostname}_${destFileName}'
+
+ftp "$sourceFile" "ftp://${ftpUser}:${ftpPass}@"${ftpSite}/${evidenceDir}/$destFile
